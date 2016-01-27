a3791aab104afc8c91cf6bc6bd9f7ad4cc5500a7
gk <shanghai_sl@hotmail.com>
2016-01-27 16:52:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum install -y gcc pcre-devel zlib-devel libxml2-devel wget tar make git
+
+#安装php
 cd /tmp
 wget http://cn2.php.net/distributions/php-5.6.17.tar.gz
 tar zxf php-5.6.17.tar.gz
@@ -7,6 +9,7 @@ cd php-5.6.17
 ./configure --prefix=/usr/local/php --enable-fpm --with-config-file-path=/usr/local/php && make && make install
 cp /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
 
+#安装nginx
 cd /tmp
 wget http://nginx.org/download/nginx-1.8.1.tar.gz
 tar zxf nginx-1.8.1.tar.gz
