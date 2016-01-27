ecbbcf00f36f5d64b407cf4639b64579f07a65fb
gk <shanghai_sl@hotmail.com>
2016-01-27 16:51:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,11 @@ cd php-5.6.17
 ./configure --prefix=/usr/local/php --enable-fpm --with-config-file-path=/usr/local/php && make && make install
 cp /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
 
+cd /tmp
+wget http://nginx.org/download/nginx-1.8.1.tar.gz
+tar zxf nginx-1.8.1.tar.gz
+cd nginx-1.8.1
+./configure && make && make install 
 
-
-
-
+/usr/local/nginx/sbin/nginx
 /usr/local/php/sbin/php-fpm
