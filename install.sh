5ca0de32c0af3168cd27c5b860e2ab94fb6e0f4d
gk <shanghai_sl@hotmail.com>
2016-01-28 12:20:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum install -y gcc pcre-devel zlib-devel libxml2-devel wget tar make git php php-fpm
+rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+yum install -y gcc pcre-devel zlib-devel libxml2-devel wget tar make git php php-fpm nginx
 
 #安装nginx
 cd /tmp
@@ -9,36 +10,36 @@ cd nginx-1.8.1
 ./configure && make && make install 
 
 #配置nginx
-echo 'worker_processes  1;' > /usr/local/nginx/conf/nginx.conf
-echo 'events {' >> /usr/local/nginx/conf/nginx.conf
-echo '    worker_connections  1024;' >> /usr/local/nginx/conf/nginx.conf
-echo '}' >> /usr/local/nginx/conf/nginx.conf
-echo 'http {' >> /usr/local/nginx/conf/nginx.conf
-echo '    include       mime.types;' >> /usr/local/nginx/conf/nginx.conf
-echo '    default_type  application/octet-stream;' >> /usr/local/nginx/conf/nginx.conf
-echo '    sendfile        on;' >> /usr/local/nginx/conf/nginx.conf
-echo '    keepalive_timeout  65;' >> /usr/local/nginx/conf/nginx.conf
-echo '    server {' >> /usr/local/nginx/conf/nginx.conf
-echo '        listen       80;' >> /usr/local/nginx/conf/nginx.conf
-echo '        server_name  localhost;' >> /usr/local/nginx/conf/nginx.conf
-echo '        location / {' >> /usr/local/nginx/conf/nginx.conf
-echo '            root   html;' >> /usr/local/nginx/conf/nginx.conf
-echo '            index  index.html index.htm;' >> /usr/local/nginx/conf/nginx.conf
-echo '        }' >> /usr/local/nginx/conf/nginx.conf
-echo '        error_page   500 502 503 504  /50x.html;' >> /usr/local/nginx/conf/nginx.conf
-echo '        location = /50x.html {' >> /usr/local/nginx/conf/nginx.conf
-echo '            root   html;' >> /usr/local/nginx/conf/nginx.conf
-echo '        }' >> /usr/local/nginx/conf/nginx.conf
-echo '        location ~ \.php$ {' >> /usr/local/nginx/conf/nginx.conf
-echo '            root           html/document/php;' >> /usr/local/nginx/conf/nginx.conf
-echo '            fastcgi_pass   127.0.0.1:9000;' >> /usr/local/nginx/conf/nginx.conf
-echo '            fastcgi_index  index.php;' >> /usr/local/nginx/conf/nginx.conf
-echo '            fastcgi_param  SCRIPT_FILENAME  /document$fastcgi_script_name;' >> /usr/local/nginx/conf/nginx.conf
-echo '            include        fastcgi_params;' >> /usr/local/nginx/conf/nginx.conf
-echo '        }' >> /usr/local/nginx/conf/nginx.conf
-echo '    }' >> /usr/local/nginx/conf/nginx.conf
-echo '}' >> /usr/local/nginx/conf/nginx.conf
-echo '' >> /usr/local/nginx/conf/nginx.conf
+echo 'worker_processes  1;' > /etc/nginx/nginx.conf
+echo 'events {' >> /etc/nginx/nginx.conf
+echo '    worker_connections  1024;' >> /etc/nginx/nginx.conf
+echo '}' >> /etc/nginx/nginx.conf
+echo 'http {' >> /etc/nginx/nginx.conf
+echo '    include       mime.types;' >> /etc/nginx/nginx.conf
+echo '    default_type  application/octet-stream;' >> /etc/nginx/nginx.conf
+echo '    sendfile        on;' >> /etc/nginx/nginx.conf
+echo '    keepalive_timeout  65;' >> /etc/nginx/nginx.conf
+echo '    server {' >> /etc/nginx/nginx.conf
+echo '        listen       80;' >> /etc/nginx/nginx.conf
+echo '        server_name  localhost;' >> /etc/nginx/nginx.conf
+echo '        location / {' >> /etc/nginx/nginx.conf
+echo '            root   html;' >> /etc/nginx/nginx.conf
+echo '            index  index.html index.htm;' >> /etc/nginx/nginx.conf
+echo '        }' >> /etc/nginx/nginx.conf
+echo '        error_page   500 502 503 504  /50x.html;' >> /etc/nginx/nginx.conf
+echo '        location = /50x.html {' >> /etc/nginx/nginx.conf
+echo '            root   html;' >> /etc/nginx/nginx.conf
+echo '        }' >> /etc/nginx/nginx.conf
+echo '        location ~ \.php$ {' >> /etc/nginx/nginx.conf
+echo '            root           html/document/php;' >> /etc/nginx/nginx.conf
+echo '            fastcgi_pass   127.0.0.1:9000;' >> /etc/nginx/nginx.conf
+echo '            fastcgi_index  index.php;' >> /etc/nginx/nginx.conf
+echo '            fastcgi_param  SCRIPT_FILENAME  /document$fastcgi_script_name;' >> /etc/nginx/nginx.conf
+echo '            include        fastcgi_params;' >> /etc/nginx/nginx.conf
+echo '        }' >> /etc/nginx/nginx.conf
+echo '    }' >> /etc/nginx/nginx.conf
+echo '}' >> /etc/nginx/nginx.conf
+echo '' >> /etc/nginx/nginx.conf
 
-/usr/local/nginx/sbin/nginx
+nginx
 php-fpm &
