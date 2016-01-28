143ac5d46c4cb0b1dfb49993f74fcdfd8df73de9
shinhwagk <191631513@qq.com>
2016-01-28 07:34:34 +0000

    f

--- php/document.php
+++ php/document.php
@@ -5,25 +5,25 @@
   <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 </head>
 <body>
-<?php require 'document_navigation_html.php' ?>
-<hr>
-<?php require 'document_breadcrumb_html.php' ?>
-<table style="width:100%">
-<!-- /* admin */ -->  <tr>
-<!-- /* admin */ -->     <td style="width:20%">
-<!-- /* admin */ --> <?php require 'document_dir_add.edit.php' ?>
-<!-- /* admin */ -->     </td>
-<!-- /* admin */ -->     <td style="width:80%">
-<!-- /* admin */ -->       <button onclick="window.location.href='/document_file_add.edit.php?path=<?php echo $s_path;?>'">新增</button>
-<!-- /* admin */ -->     </td>
-<!-- /* admin */ -->  </tr>
-  <tr>
-     <td>
-       <?php require 'document_dir_list_html.php' ?>
-     </td>
-    <td style="BORDER-LEFT: 1px solid;padding:15px;">
-      <?php require 'document_content_html.php' ?>
-    </td>
+  <?php require 'document_navigation_html.php' ?>
+  <hr>
+  <?php require 'document_breadcrumb_html.php' ?>
+  <table style="width:100%">
+    <tr>
+      <td style="width:20%">
+        <?php require 'document_dir_add.edit.php' ?>
+      </td>
+      <td style="width:80%">
+        <button onclick="window.location.href='/document_file_add.edit.php?path=<?php echo $s_path;?>'">新增</button>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <?php require 'document_dir_list_html.php' ?>
+      </td>
+      <td style="BORDER-LEFT: 1px solid;padding:15px;">
+        <?php require 'document_content_html.php' ?>
+      </td>
   </tr>
 </table>
 </body>
