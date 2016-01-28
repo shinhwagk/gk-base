0e2a2e16b12942b755b2b9c6973466274d8938c8
shinhwagk <191631513@qq.com>
2016-01-28 08:24:15 +0000

    a

--- php/document.php
+++ php/document.php
@@ -1,7 +1,7 @@
 <?php $s_path = $_GET["path"]; ?>
 <html>
 <head>
-  <script src="http://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.0.min.js" charset="utf-8"></script>
   <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 </head>
 <body>
